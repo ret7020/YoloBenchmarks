0ccbd89eda1a582548eaf7d8df425bf00dfe0e1c
ret7020 <ret7020@gmail.com>
2024-06-10 23:09:27 +0300

    Configs for all openvino models

--- server/config.py
+++ server/config.py
@@ -7,9 +7,25 @@ models = {
         ("yolov8x.pt", "1stXAXeDSgaMFv8AkZwtiw5RXvInd74tQ", False),
     ],
     "openvino": [
-        ("yolov8n_openvino_base_openvino_model", "1_NAjGu9S9_n4z2vw3S65zHFQvmx0ZEVc", True),
+        ("yolov8n_openvino_base_openvino_model", "1A0O8iK23T9x0yX5sEy-Hvd0j_ZAttCKb", True),
         ("yolov8n_openvino_half_openvino_model", "1B2FSJv5RV-fc0YsX10n6dBaJvC-eOCEq", True),
-        ("yolov8n_openvino_int8_openvino_model", "18PyBs5ktB46nJCnv6PcEaHFZ_p4DRM9U", True)
+        ("yolov8n_openvino_int8_openvino_model", "18PyBs5ktB46nJCnv6PcEaHFZ_p4DRM9U", True),
+
+        ("yolov8s_openvino_base_openvino_model", "1ejRBsLHUHOGK8sda_bXdJd_PPUDkDhpx", True),
+        ("yolov8s_openvino_half_openvino_model", "14_dFDo4rnumxA7nzGXwS5tg1sSHL3Hq6", True),
+        ("yolov8s_openvino_int8_openvino_model", "1P3R7Nu3M0bYz_8OiHuyopQ8TWLK4V6Sz", True),
+
+        ("yolov8m_openvino_base_openvino_model", "1a4mqHICsVL48i7LXmosqS0bOLuITB6s9", True),
+        ("yolov8m_openvino_half_openvino_model", "1sltYBHF1yVmw8QhO9ZIXrINxR3ekbu0z", True),
+        ("yolov8m_openvino_int8_openvino_model", "1buMeXWTHHxVrT5o9VgYWE1zjPxtpbvVB", True),
+
+        ("yolov8l_openvino_base_openvino_model", "1_NAjGu9S9_n4z2vw3S65zHFQvmx0ZEVc", True),
+        ("yolov8l_openvino_half_openvino_model", "12sWjisIBAgotkb7VjUqwTNstK5wutptK", True),
+        ("yolov8l_openvino_int8_openvino_model", "1-0TYWUYt6CCl99nMdqLNUH12QCqXFQ3T", True),
+
+        ("yolov8x_openvino_base_openvino_model", "1auiTSisn2SAbws2C3yiC_fJf5Yb4Tv3b", True),
+        ("yolov8x_openvino_half_openvino_model", "1xlaM7NQR2YcJEDaJt9bxVu-Q7qwEdvwQ", True),
+        ("yolov8x_openvino_int8_openvino_model", "1Pju0ySDnFZ2AxySmoZTqQJT0wxQy2Iqs", True),
     ],
     "tensorRT": ["CUDAyolov8n.pt"]
 }
